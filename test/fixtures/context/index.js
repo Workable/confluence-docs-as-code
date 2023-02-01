02f7171298c62ab4a1b0c329e67dd03a9a4ef075
Eleftherios Chetzakis <eleftherios@workable.com>
2023-02-01 10:12:43 +0200

    Prepend titles with configured prefix

--- test/fixtures/context/index.js
+++ test/fixtures/context/index.js
@@ -3,5 +3,6 @@ const base = 'test/fixtures/context';
 const simple = JSON.parse(readFileSync(`${base}/simple.json`));
 const noReadme = JSON.parse(readFileSync(`${base}/without-readme.json`));
 const withUnsafeFiles = JSON.parse(readFileSync(`${base}/with-unsafe-paths.json`));
+const withTitlePrefix = JSON.parse(readFileSync(`${base}/with-prefix.json`));
 
-export { simple, noReadme, withUnsafeFiles };
+export { simple, noReadme, withUnsafeFiles, withTitlePrefix };
