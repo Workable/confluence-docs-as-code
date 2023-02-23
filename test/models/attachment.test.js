0dd1b60bb8e74f7276bb88080f1a7b451bdb8456
Eleftherios Chetzakis <eleftherios@workable.com>
2023-02-23 15:15:09 +0200

    Attachment model

---
+++ test/models/attachment.test.js
@@ -0,0 +1,26 @@
+import Attachment from '../../lib/models/attachment.js';
+import { expect } from 'chai';
+
+describe('models/attachment', () => {
+    const file = 'file.ext';
+    const path = `foo/bar/${file}`;
+    let attachment;
+    beforeEach(() => {
+        attachment = new Attachment(path);
+    });
+    describe('constructor', () => {
+        it('should set path', () => {
+            attachment.path.should.be.equal(path);
+        });
+    });
+    describe('filename', () => {
+        it('should return the basename from the path', () => {
+            attachment.filename.should.be.equal(file);
+        });
+    });
+    describe('render', () => {
+        it('should throw exception', () => {
+            expect(() => attachment.render()).to.throw('Not implemented!');
+        });
+    });
+});
